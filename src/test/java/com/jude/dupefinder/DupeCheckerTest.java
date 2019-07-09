40ba932fff4ae1b8e2d0995ebfef909bd0fee456
Jude Cooray <ranhiru@gmail.com>
2019-07-09 11:38:55 +1000

    Added first test

---
+++ src/test/java/com/jude/dupefinder/DupeCheckerTest.java
@@ -0,0 +1,12 @@
+package com.jude.dupefinder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DupeCheckerTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
